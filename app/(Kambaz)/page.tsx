7eb6b60c9af5e88d7ebf210c2cbd44883985086a
isabel-ann <104743014+isabel-ann@users.noreply.github.com>
2025-10-02 21:32:51 -0400

    Fixed main redirect

--- app/(Kambaz)/page.tsx
+++ app/(Kambaz)/page.tsx
@@ -1,9 +1,6 @@
+
+import { redirect } from 'next/navigation';
+
 export default function Courses() {
-  return (
-    <div id="wd-courses">
-      <h2>Course 1234</h2>
-    </div>
-    
-);}
-
-
+  redirect('/Dashboard');
+}
