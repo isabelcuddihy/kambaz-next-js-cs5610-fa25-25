842e86387ad12bfdb61b87802613d5d051b7e2b4
isabel-ann <104743014+isabel-ann@users.noreply.github.com>
2025-10-15 14:03:26 -0400

    Build fixes

--- app/Labs/Lab3/AddingAndRemovingToFromArrays.tsx
+++ app/Labs/Lab3/AddingAndRemovingToFromArrays.tsx
@@ -4,7 +4,7 @@ export default function AddingAndRemovingToFromArrays() {
   const todoArray = [<li key={"buy-milk"}>Buy milk</li>, <li key={"feed-pet"}>Feed the pets</li>];
   numberArray1.push(6); // adding new items
   stringArray1.push("string3");
-  todoArray.push(<li>Walk the dogs</li>);
+  todoArray.push(<li key={"walk-dog"}>Walk the dogs</li>);
   numberArray1.splice(2, 1); // remove 1 item starting at 2
   stringArray1.splice(1, 1);
   return (
