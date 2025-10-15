23c897cb8d30ddbfde487439ffc9418f34adc6d1
isabel-ann <104743014+isabel-ann@users.noreply.github.com>
2025-10-15 13:46:00 -0400

    Build fixes

--- app/(Kambaz)/Courses/[cid]/Navigation.tsx
+++ app/(Kambaz)/Courses/[cid]/Navigation.tsx
@@ -2,26 +2,42 @@
   "use client"
 import Link from "next/link";
 import { useParams } from "next/navigation";
+import { usePathname } from "next/navigation";
 
 export default function CourseNavigation() {
   const { cid } = useParams();
+   const pathname = usePathname();
   
   return (
     <div id="wd-courses-navigation" className="wd list-group fs-5 rounded-0">
       <Link href={`/Courses/${cid}/Home`} id="wd-course-home-link"
-        className="list-group-item active border-0"> Home </Link>
+        className={pathname.includes("Home")
+  ? "list-group-item active border-0" 
+  : "list-group-item text-danger border-0"}> Home </Link>
       <Link href={`/Courses/${cid}/Modules`} id="wd-course-modules-link"
-        className="list-group-item text-danger border-0"> Modules </Link>
+         className={pathname.includes("Modules")
+  ? "list-group-item active border-0" 
+  : "list-group-item text-danger border-0"}> Modules </Link>
       <Link href={`/Courses/${cid}/Piazza`} id="wd-course-piazza-link"
-        className="list-group-item text-danger border-0"> Piazza </Link>
+        className={pathname.includes("Piazza")
+  ? "list-group-item active border-0" 
+  : "list-group-item text-danger border-0"}> Piazza </Link>
       <Link href={`/Courses/${cid}/Zoom`} id="wd-course-zoom-link"
-        className="list-group-item text-danger border-0"> Zoom </Link>
+        className={pathname.includes("Zoom")
+  ? "list-group-item active border-0" 
+  : "list-group-item text-danger border-0"}> Zoom </Link>
       <Link href={`/Courses/${cid}/Assignments`} id="wd-course-assignments-link"
-        className="list-group-item text-danger border-0"> Assignments </Link>
+       className={pathname.includes("Assignments")
+  ? "list-group-item active border-0" 
+  : "list-group-item text-danger border-0"}> Assignments </Link>
       <Link href={`/Courses/${cid}/Quizzes`} id="wd-course-quizzes-link"
-        className="list-group-item text-danger border-0"> Quizzes </Link>
+        className={pathname.includes("Quizzes")
+  ? "list-group-item active border-0" 
+  : "list-group-item text-danger border-0"}> Quizzes </Link>
       <Link href={`/Courses/${cid}/People/Table`} id="wd-course-people-link"
-        className="list-group-item text-danger border-0"> People </Link>
+        className={pathname.includes("People")
+  ? "list-group-item active border-0" 
+  : "list-group-item text-danger border-0"}> People </Link>
     </div>
   );
 }
