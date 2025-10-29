723ccb6cf16a344bf503d085fbf8520770deafa1
isabel-ann <104743014+isabel-ann@users.noreply.github.com>
2025-10-29 13:00:10 -0400

    None enrolled course access blocker

--- app/(Kambaz)/Courses/[cid]/layout.tsx
+++ app/(Kambaz)/Courses/[cid]/layout.tsx
@@ -1,14 +1,36 @@
 "use client";
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 import CourseNavigation from "./Navigation";
 import { FaAlignJustify } from "react-icons/fa6";
 import { courses } from "../../Database";
 import { useSelector } from "react-redux";
-import { useParams } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
+
 export default function CoursesLayout({ children }: { children: ReactNode }) {
-  const { cid } = useParams();
- const { courses } = useSelector((state: any) => state.coursesReducer);
- const course = courses.find((course: any) => course._id === cid);
+const { cid } = useParams();
+  const router = useRouter();
+  const { currentUser } = useSelector((state: any) => state.accountReducer);
+  const { enrollments } = useSelector((state: any) => state.enrollmentReducer);
+  const { courses } = useSelector((state: any) => state.coursesReducer);
+  
+  const course = courses.find((course: any) => course._id === cid);
+ // Check if user is enrolled
+  const isEnrolled = enrollments.some(
+    (enrollment: any) =>
+      enrollment.user === currentUser._id && enrollment.course === cid
+  );
+
+  useEffect(() => {
+    if (!isEnrolled) {
+      // Not enrolled - send user to Dashboard
+      router.push("/Dashboard");
+    }
+  }, [isEnrolled, router]);
+
+  // Don't access if not enrolled
+  if (!isEnrolled) {
+    return null;
+  }
   return (
     <div id="wd-courses">
       <h2 className="text-danger">
